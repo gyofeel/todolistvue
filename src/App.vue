f72b4e8562651d76c3235830363a9b3c9237e138
서교필 <gyofeel318@gmail.com>
2019-04-24 22:45:49 +0900

    add App vue & fix functions of this app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <Todo></Todo>
+    <Done></Done>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from "./components/Header.vue";
+import Todo from "./components/Todo.vue";
+import Done from "./components/Done.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
+  name: "app",
+  data() {
+    return {
+      todoItems: [],
+      doneItems: []
+    };
   },
+  methods: {
+    addTodo(item) {
+      localStorage.todoItems.setItem(item);
+      this.todoItems.push(item);
+    },
+    addDone(item) {
+      // remove
+      localStorage.todoItems.removeItem(item);
+      const idx = this.todoItems.findIndex(o => item);
+      this.todoItems.splice(idx, 1);
+
+      // add
+      localStorage.setItem(done, item);
+      this.doneItems.push(item);
+    },
+    removeTodo(item) {
+      localStorage.todoItems.removeItem(item);
+      const idx = this.todoItems.findIndex(o => item);
+      this.todoItems.splice(idx, 1);
+    },
+    cancelDone(item) {
+      localStorage.doneItems.removeItem(item);
+      const idx = this.doneItems.findIndex(o => item);
+      this.doneItems.splice(idx, 1);
+    },
+    created() {
+      if (localStorage.todoItems.length > 0) {
+        for (let el of localStorage.todoItems) {
+          this.todoItems.push(el);
+        }
+      }
+      if (localStorage.doneItems.length > 0) {
+        for (let el of localStorage.doneItems) {
+          this.doneItems.push(el);
+        }
+      }
+    }
+  },
+  components: {
+    Header,
+    Todo,
+    Done
+  }
 };
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica, "Avenir", Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+  background-color: #dff9fb;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
 }
 </style>
